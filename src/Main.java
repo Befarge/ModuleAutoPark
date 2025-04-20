2afddc2fce207a72463af750460f970eafaba110
= <venikov2011@yandex.ru>
2025-04-20 10:59:17 +0300

    Создание сущности для пользователя

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,11 @@
-import javax.swing.*;
-import java.awt.*;
-import java.sql.*;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
+import entity.User;
 
 public class Main {
     public static void main(String[] args) {
-        //Создаем объект Property для выборки данных из файла
-        Properties properties = new Properties();
-
-        // GUI: создаём окно
-        JFrame frame = new JFrame("Данные из базы");
-        JTextArea textArea = new JTextArea(20, 40);
-        textArea.setEditable(false);
-        frame.add(new JScrollPane(textArea), BorderLayout.CENTER);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        try (FileInputStream input = new FileInputStream("config.properties")) {
-            properties.load(input);
-            Connection conn = DriverManager.getConnection(
-                    properties.getProperty("db.url"),
-                    properties.getProperty("db.username"),
-                    properties.getProperty("db.password")
-            );
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-
-            ResultSetMetaData meta = rs.getMetaData();
-            int columnCount = meta.getColumnCount();
-
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    textArea.append(meta.getColumnName(i) + ": " + rs.getString(i) + "\n");
-                }
-                textArea.append("-----------\n");
-            }
-        } catch (SQLException e) {
-            textArea.setText("Ошибка подключения к базе:\n" + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            textArea.setText("Ошибка c конфигурационным файлом:\n" + e.getMessage());
-            e.printStackTrace();
-        }
+        User user1 = new User(1,"admin", "ven741");
+        System.out.println(user1.getId());
+        user1.setId(2);
+        System.out.println(user1.getId());
+        System.out.println(user1.getPassword());
     }
 }
---
+++ src/entity/User.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class User {
+    // Поля для пользователя
+    private int id;
+    private String login;
+    private String password;
+
+    // Конструктор
+    public User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    // Геттеры и сеттеры
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
