b3c5fb28cbb017eef1537ff521a1bffe693e38ce
= <venikov2011@yandex.ru>
2025-04-21 23:30:18 +0300

    Изменение типа для user_id. int -> Integer

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import entity.User;
 
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User(1,"admin", "ven741");
+        User user1 = new User("admin", "ven741");
         System.out.println(user1.getId());
-        user1.setId(2);
-        System.out.println(user1.getId());
-        System.out.println(user1.getPassword());
     }
 }
