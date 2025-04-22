3a909b0ff4d126c1ef9321db92cde04e915c8a88
= <venikov2011@yandex.ru>
2025-04-22 18:02:16 +0300

    Добавления вывода о пользователе для удобства отладки

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import types.UserRole;
 
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User("admin", "ven741", UserRole.USER);
-        System.out.println(user1.getRole());
+        User user1 = new User(2,"admin", "ven741", UserRole.ADMIN);
+        user1.printInfo();
     }
 }
