7b954386f756aa2d1592ffd8f65faac4a55039e7
= <venikov2011@yandex.ru>
2025-04-22 23:25:54 +0300

    Создание сущности для машины

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,15 @@
-import javax.swing.*;
-import java.awt.*;
-import java.sql.*;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
+import entity.Car;
 
 public class Main {
     public static void main(String[] args) {
-        //Создаем объект Property для выборки данных из файла
-        Properties properties = new Properties();
+        Car car1 = new Car(
+                "Nissan Almera",
+                "О170СМ",
+                1000,
+                50,
+                "10-03-25"
+        );
 
-        // GUI: создаём окно
-        JFrame frame = new JFrame("Данные из базы");
-        JTextArea textArea = new JTextArea(20, 40);
-        textArea.setEditable(false);
-        frame.add(new JScrollPane(textArea), BorderLayout.CENTER);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        try (FileInputStream input = new FileInputStream("config.properties")) {
-            properties.load(input);
-            Connection conn = DriverManager.getConnection(
-                    properties.getProperty("db.url"),
-                    properties.getProperty("db.username"),
-                    properties.getProperty("db.password")
-            );
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-
-            ResultSetMetaData meta = rs.getMetaData();
-            int columnCount = meta.getColumnCount();
-
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    textArea.append(meta.getColumnName(i) + ": " + rs.getString(i) + "\n");
-                }
-                textArea.append("-----------\n");
-            }
-        } catch (SQLException e) {
-            textArea.setText("Ошибка подключения к базе:\n" + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            textArea.setText("Ошибка c конфигурационным файлом:\n" + e.getMessage());
-            e.printStackTrace();
-        }
+        car1.printInfo();
     }
 }
---
+++ src/entity/Car.java
@@ -0,0 +1,98 @@
+package entity;
+
+public class Car {
+    private Integer car_id;
+    private String model;
+    private String licensePlate;
+    private int mileage;
+    private int fuelLevel;
+    private String lastMaintenanceDate;
+    private boolean isAvailable;
+
+    public Car(int car_id, String model, String licensePlate, int mileage, int fuelLevel, String lastMaintenanceDate, boolean isAvailable) {
+        this.car_id = car_id;
+        this.model = model;
+        this.licensePlate = licensePlate;
+        this.mileage = mileage;
+        this.fuelLevel = fuelLevel;
+        this.lastMaintenanceDate = lastMaintenanceDate;
+        this.isAvailable = isAvailable;
+    }
+
+    public Car(String model, String licensePlate, int mileage, int fuelLevel, String lastMaintenanceDate) {
+        this.model = model;
+        this.licensePlate = licensePlate;
+        this.mileage = mileage;
+        this.fuelLevel = fuelLevel;
+        this.lastMaintenanceDate = lastMaintenanceDate;
+    }
+
+    // Getters & Setters
+
+    public int getId() {
+        return car_id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(int fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public String getLastMaintenanceDate() {
+        return lastMaintenanceDate;
+    }
+
+    public void setLastMaintenanceDate(String lastMaintenanceDate) {
+        this.lastMaintenanceDate = lastMaintenanceDate;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public void printInfo() {
+        System.out.printf("""
+              car_id = %d
+              model = %s
+              licensePlate = %s
+              mileage = %d
+              fuelLevel = %d
+              lastMaintenanceDate = %s
+              isAvailable = %b
+              
+              """ ,
+              car_id, model, licensePlate, mileage, fuelLevel, lastMaintenanceDate, isAvailable
+        );
+    }
+}
