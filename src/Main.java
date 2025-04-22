b15da6aad4dfdbe935d854c383a624bb0ca27fb5
= <venikov2011@yandex.ru>
2025-04-22 22:13:54 +0300

    Добавление базового функционала, со временем можно что-то еще добавить

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
+import dao.DriverDAO;
 import entity.Driver;
+import db.ConfigReader;
+import db.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        ConfigReader config = new ConfigReader("config.properties");
+        DatabaseConnection db = new DatabaseConnection(config);
+        Connection connect = db.connect();
+        DriverDAO dao = new DriverDAO(connect);
+
         Driver driver1 = new Driver(
                 "Евгений",
                 "Александрович",
                 "Веников",
                 19,
-                "89611722076",
+                "89611722075",
                 1
         );
 
-        driver1.printInfo();
+        //dao.addDriver(driver1);
+        //dao.getDriverById(2).printInfo();
+        //dao.updateDriver(driver1, 2);
+        //dao.deleteDriver(2);
+        //dao.getDriverByUserId(1).printInfo();
+        dao.getDriverByPhoneNumber("89611722076").printInfo();
+
+        db.release();
+        connect.close();
     }
 }
---
+++ src/dao/DriverDAO.java
@@ -0,0 +1,146 @@
+package dao;
+import entity.Driver;
+import java.sql.*;
+
+public class DriverDAO {
+    private final Connection connection;
+
+    public DriverDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void addDriver(Driver driver) {
+        String sql = "INSERT INTO drivers (last_name, first_name, middle_name, age, phone_number, on_trip, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, driver.getLastName());
+            stmt.setString(2, driver.getFirstName());
+            stmt.setString(3, driver.getMiddleName());
+            stmt.setInt(4, driver.getAge());
+            stmt.setString(5, driver.getPhoneNumber());
+            stmt.setBoolean(6, driver.isOnTrip());
+            stmt.setInt(7, driver.getUserId());
+            if (stmt.executeUpdate() > 0)
+                System.out.println("Добавление прошло успешно");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Driver getDriverById(int driver_id) {
+        String sql = "SELECT * FROM drivers WHERE driver_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, driver_id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Driver(
+                        driver_id,
+                        rs.getInt("user_id"),
+                        rs.getString("first_name"),
+                        rs.getString("middle_name"),
+                        rs.getString("last_name"),
+                        rs.getInt("age"),
+                        rs.getString("phone_number"),
+                        rs.getBoolean("on_trip")
+                );
+            } else {
+                System.out.println("Водитель не был найден");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Driver getDriverByUserId (int user_id) {
+        String sql = "SELECT * FROM drivers WHERE user_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, user_id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Driver(
+                        rs.getInt("driver_id"),
+                        user_id,
+                        rs.getString("first_name"),
+                        rs.getString("middle_name"),
+                        rs.getString("last_name"),
+                        rs.getInt("age"),
+                        rs.getString("phone_number"),
+                        rs.getBoolean("on_trip")
+                );
+            } else {
+                System.out.println("Водитель не был найден");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Driver getDriverByPhoneNumber (String phoneNumber) {
+        String sql = "SELECT * FROM drivers WHERE phone_number = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, phoneNumber);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Driver(
+                        rs.getInt("driver_id"),
+                        rs.getInt("user_id"),
+                        rs.getString("first_name"),
+                        rs.getString("middle_name"),
+                        rs.getString("last_name"),
+                        rs.getInt("age"),
+                        phoneNumber,
+                        rs.getBoolean("on_trip")
+                );
+            } else {
+                System.out.println("Водитель не был найден");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void updateDriver(Driver driver, int driver_id) {
+        String sql = """
+                UPDATE drivers SET
+                last_name = ?,
+                first_name = ?,
+                middle_name = ?,
+                age = ?,
+                phone_number = ?,
+                on_trip = ?,
+                user_id = ?
+                WHERE driver_id = ?
+                """;
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, driver.getLastName());
+            stmt.setString(2, driver.getFirstName());
+            stmt.setString(3, driver.getMiddleName());
+            stmt.setInt(4, driver.getAge());
+            stmt.setString(5, driver.getPhoneNumber());
+            stmt.setBoolean(6, driver.isOnTrip());
+            stmt.setInt(7, driver.getUserId());
+            stmt.setInt(8, driver_id);
+            if (stmt.executeUpdate() > 0)
+                System.out.println("Обновление прошло успешно");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteDriver(int driver_id) {
+        String sql = "DELETE FROM drivers WHERE driver_id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, driver_id);
+            if (stmt.executeUpdate() > 0) {
+                System.out.println("Удаление прошло успешно");
+            } else {
+                System.out.println("Ничего не было удалено");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
