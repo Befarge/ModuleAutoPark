a0766dfedfde987c81ededc11da9aed8d738a603
= <venikov2011@yandex.ru>
2025-04-23 19:21:49 +0300

    Реализация основных методов

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
+import dao.TripDAO;
+import db.ConfigReader;
+import db.DatabaseConnection;
 import entity.Trip;
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Trip trip1 = new Trip (
-                1,
-                2
+    public static void main(String[] args) throws SQLException {
+        ConfigReader config = new ConfigReader("config.properties");
+        DatabaseConnection db = new DatabaseConnection(config);
+        Connection connect = db.connect();
+        TripDAO md = new TripDAO(connect);
+
+        Trip trip1 = new Trip(
+                4,
+                1
         );
 
-        trip1.printInfo();
+//        md.addTrip(trip1);
+//        Trip new_trip = md.getTripById(4);
+//        md.deleteTrip(3);
+//        new_trip.setFuelUsed(49);
+//        new_trip.setDistance(100);
+//
+//        md.updateTrip(new_trip,4);
+
+        md.getTripByDriverId(4).printInfo();
+
+        db.release();
+        connect.close();
     }
 }
