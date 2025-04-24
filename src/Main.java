48396402194346289e9d0d907446cc3d0165eb8b
= <venikov2011@yandex.ru>
2025-04-24 13:50:53 +0300

    Перенос всей логики в DatabaseConnection

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,12 @@
-import dao.UserDAO;
 import db.ConfigReader;
 import db.DatabaseConnection;
-import entity.User;
-import types.UserRole;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         ConfigReader config = new ConfigReader("config.properties");
         DatabaseConnection db = new DatabaseConnection(config);
-        Connection connect = db.connect();
-        UserDAO md = new UserDAO(connect);
-        User user1 = new User("nekr", "2110", UserRole.MANAGER);
-        //md.addUser(user1);
-        //md.deleteUser(6);
-        //System.out.println(md.getUserById(11).getLogin());
-        //md.getUserByLogin("venikov").printInfo();
 
-        System.out.println(md.addUser(user1));
-
-        db.release();
-        connect.close();
+        db.getDriverDAO().getDriverById(4).printInfo();
+        db.close();
     }
 }
